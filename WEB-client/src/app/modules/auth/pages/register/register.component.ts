e62df1e0a6ee1e81d2b7da446591b6ba39a286ef
SixDevelop <47978391+SixDevelop@users.noreply.github.com>
2020-10-05 16:59:36 +0300

    issues9/ add html/css for reg page, fill reg component

--- WEB-client/src/app/modules/auth/pages/register/register.component.ts
+++ WEB-client/src/app/modules/auth/pages/register/register.component.ts
@@ -1,4 +1,7 @@
+import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private router: Router, private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      username: ["", Validators.required],
+      password: ["", Validators.required]
+    });
+  }
+
+  get form(){
+    return this.registerForm.controls;
+  }
+
+  onSubmit(){
+    if (this.registerForm.invalid){
+      console.log("form is invalid");
+      return;
+    }
+
+    console.log(this.form.username, this.form.password)
+
+    this.authService.register( this.form.username.value, this.form.password.value )
+    this.router.navigate(['/'])
   }
 
 }
