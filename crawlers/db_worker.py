b15b391530e5d42a2d8541346ea805e37e5082d0
exp101t <ritter.german@yandex.ru>
2020-10-07 21:05:22 +0300

    db worker update

--- crawlers/db_worker.py
+++ crawlers/db_worker.py
@@ -3,6 +3,7 @@ from config import (
     DB_PASS, DB_PORT
 )
 from datetime import datetime
+from typing import List
 import psycopg2
 
 connection = psycopg2.connect(
@@ -14,32 +15,64 @@ connection = psycopg2.connect(
 )
 
 cursor = connection.cursor()
-city_ids = dict()
+table_cache = dict()
 
-def add_event(event_time: datetime,
-              city: str,
-              description: str,
-              place: str,
-              logo_path: str):
-    """ Add event to database, but do not commit changes """
+def select_or_insert(table: str, column: str, value: str):
+    """ 
+        Find entry in list-like table if provided value exists in that table
+        Create new one if it doesn't
+    """
 
-    if city in city_ids.keys():
-        city_id = city_ids[city]
+    select_query = 'SELECT id FROM {} '\
+                   'WHERE {}=%s'.format(table, column)
+    insert_query = 'INSERT INTO {} ({}) '\
+                   'VALUES (%s) RETURNING id'.format(table, column)
+
+    if table not in table_cache:
+        table_cache[table] = dict()
+
+    id_cache = table_cache[table]
+
+    if value in id_cache:
+        return id_cache[value]
     else:
-        cursor.execute("SELECT id FROM cities WHERE city=%s", (city,))
+        cursor.execute(select_query, (value,))
         entry = cursor.fetchone()
 
         if entry is None:
-            cursor.execute("INSERT INTO cities(city) VALUES (%s) RETURNING id",
-                           (city,))
+            cursor.execute(insert_query, (value,))
             entry = cursor.fetchone()
 
-        city_id = city_ids[city] = entry[0]
+        id_cache[value] = entry[0]
+        return entry[0]
 
-    cursor.execute("INSERT INTO events(event_time, city, description, "\
-                   "place, logo_path) VALUES (%s, %s, %s, %s, %s)",
-                   (event_time, city_id, description, place, logo_path))
+def add_event(name: str, event_time: datetime, city: str, place: str,
+              source_url: str, description: str,
+              categories: List[str] = [],
+              logo_path: str = None):
+    """
+        Add event to database, but do not commit changes
+    """
+
+    event_insert_query = 'INSERT INTO events (name, event_time, city, '\
+                         'place, source_url, description, logo_path) '\
+                         'VALUES (%s, %s, %s, %s, %s, %s, %s) RETURNING id'
+    links_insert_query = 'INSERT INTO event_category_links (event_id, '\
+                         'category_id) VALUES (%s, %s)'
+
+    city_id = select_or_insert('cities', 'city', city)
+
+    cursor.execute(event_insert_query, (name, event_time, city_id, place, 
+                                        source_url, description, logo_path))
+    event_id = cursor.fetchone()[0]
+
+    for category in categories:
+        category_id = select_or_insert('categories', 'category', category)
+        cursor.execute(links_insert_query, (event_id, category_id))
 
 def commit_changes():
-    """ Commit changes to database """
+    """
+        Commit changes to database
+    """
+
     connection.commit()
