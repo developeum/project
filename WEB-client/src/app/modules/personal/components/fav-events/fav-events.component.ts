f99a2e982e57a253d64657aa32934237e3afae7f
SixDevelop <47978391+SixDevelop@users.noreply.github.com>
2020-10-19 10:30:57 +0300

    issues21/init components and service

---
+++ WEB-client/src/app/modules/personal/components/fav-events/fav-events.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fav-events',
+  templateUrl: './fav-events.component.html',
+  styleUrls: ['./fav-events.component.scss']
+})
+export class FavEventsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- WEB-client/src/app/modules/personal/personal.module.ts
+++ WEB-client/src/app/modules/personal/personal.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PersonalMainComponent } from './pages/personal-main/personal-main.component';
+import { UserInfoComponent } from './components/user-info/user-info.component';
+import { FavEventsComponent } from './components/fav-events/fav-events.component';
 
 
 
 @NgModule({
-  declarations: [PersonalMainComponent],
+  declarations: [PersonalMainComponent, UserInfoComponent, FavEventsComponent],
   imports: [
     CommonModule
   ]
