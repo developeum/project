32116ac3439bf55a903548d266ac99b0579227b7
alinadmb <43730690+alinadmb@users.noreply.github.com>
2020-10-21 00:52:35 +0300

    added stopwords and got rid of english words in description

--- preprocessing_events_data.py
+++ preprocessing_events_data.py
@@ -1,7 +1,10 @@
 import pandas
 import pymorphy2
 import numpy as np
-import re
+import nltk
+nltk.download("stopwords")
+from nltk.corpus import stopwords
+import langdetect
 
 events_df = pandas.read_csv('meetup_tech_18102020_tz_fixed.csv')
 
@@ -17,26 +20,29 @@ events_df['name'] = events_df['name'].replace(r'\w*январ\w*|\w*феврал
 events_df['name'] = events_df['name'].replace(r'[^а-яА-Яa-zA-Z]', ' ', regex=True)
 
 morph = pymorphy2.MorphAnalyzer()
+
+stopwords = stopwords.words("russian")
+stopwords.extend(['что', 'это', 'весь', 'этот', 'привет', 'так', 'вот', 'как', 'ссылка', 'регистрация', 'приглашать', 'еще'])
 descr_array = []
 names_array = []
+
 for i in range(len(events_df)):
     descr = events_df['description'][i]
     if not pandas.isna(descr):
-        descr = [morph.parse(word)[0].normal_form for word in descr.split() if morph.parse(word)[0].tag.POS not in {'INTJ', 'PRCL', 'CONJ', 'PREP', 'NPRO'}]
-        if descr == ['регистрация'] or descr == ['ссылка', 'регистрация']:
+        descr = [morph.parse(word)[0].normal_form for word in descr.split() if morph.parse(word)[0].tag.POS not in {'INTJ', 'PRCL', 'CONJ', 'PREP', 'NPRO'}
+                 and morph.parse(word)[0].normal_form not in stopwords and langdetect.detect(word) == 'ru']
+        if not descr:
             descr = np.nan
         else:
             descr = ' '.join(descr)
     descr_array.append(descr)
     name = events_df['name'][i]
     if not pandas.isna(name):
-        name = [morph.parse(word)[0].normal_form for word in name.split() if morph.parse(word)[0].tag.POS not in {'INTJ', 'PRCL', 'CONJ', 'PREP', 'NPRO'}]
+        name = [morph.parse(word)[0].normal_form for word in name.split() if morph.parse(word)[0].tag.POS not in {'INTJ', 'PRCL', 'CONJ', 'PREP', 'NPRO'}
+                and morph.parse(word)[0].normal_form not in stopwords]
         name = ' '.join(name)
     names_array.append(name)
 events_df['name'] = names_array
 events_df['description'] = descr_array
-
-events_df['description'] = events_df['description'].replace(r'\bвесь\b|\bвс.\b|\bэтот\b|\bэт.\b|\bпривет\b', '', regex=True)
-events_df['name'] = events_df['name'].replace(r'\bвесь\b|\bвс.\b|\bэтот\b|\bэт.\b', '', regex=True)
 
 events_df.to_csv('events18102020.csv')
