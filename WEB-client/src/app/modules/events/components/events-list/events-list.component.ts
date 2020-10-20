9d884562aee6407bcef78b8041b6d07b7f2e1db6
SixDevelop <47978391+SixDevelop@users.noreply.github.com>
2020-10-20 13:00:10 +0300

    issues22/add simple html/css

--- WEB-client/src/app/modules/events/components/events-list/events-list.component.ts
+++ WEB-client/src/app/modules/events/components/events-list/events-list.component.ts
@@ -1,3 +1,5 @@
+import { EventsService } from './../../servises/events.service';
+import { EventFL } from './../../../../models/eventsFL';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./events-list.component.scss']
 })
 export class EventsListComponent implements OnInit {
+  userId: string;
+  events: EventFL[];
 
-  constructor() { }
+  constructor(private pageService: EventsService) { }
 
   ngOnInit(): void {
+  }
+
+  loadRandEvents(){
+    this.pageService.getEvents().subscribe(x => {
+      this.events = x;
+    })
+  }
+
+  loadRecEvents(){
+    this.pageService.getRec(this.userId).subscribe(x => {
+      this.events = x;
+    })
   }
 
 }
