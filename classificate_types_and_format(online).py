bbda04826c105938f3326e62ffc8735970701b52
alinadmb <43730690+alinadmb@users.noreply.github.com>
2020-10-23 23:14:48 +0300

    added stopwords

--- classificate_types_and_format(online).py
+++ classificate_types_and_format(online).py
@@ -1,34 +1,40 @@
-import pandas
-import numpy as np
-pandas.options.mode.chained_assignment = None
+def classificate_types_and_format(csv_in):
+    import pandas
+    import numpy as np
+    pandas.options.mode.chained_assignment = None
 
-events_df = pandas.read_csv('events18102020.csv')
-events_df['type'] = np.nan
+    events_df = pandas.read_csv(csv_in)
+    events_df['type'] = np.nan
 
-hackathon = ['hackathon', 'хакатон']
-webinar = ['webinar', 'вебинар', 'lecture', 'лекция']
-conference = ['conference', 'конференция']
-training = ['training', 'тренинг', 'семинар', 'workshop', 'мастер класс']
-course = ['course', 'курс']
-meetup = ['meetup', 'митап']
+    hackathon = ['hackathon', 'хакатон']
+    webinar = ['webinar', 'вебинар', 'lecture', 'лекция']
+    conference = ['conference', 'конференция']
+    training = ['training', 'тренинг', 'семинар', 'workshop', 'мастер класс']
+    course = ['course', 'курс']
+    meetup = ['meetup', 'митап']
 
-types = ['hackathon', 'webinar', 'conference', 'training', 'course', 'meetup', 'others']
-keywords = [hackathon, webinar, conference, training, course, meetup]
+    types = ['hackathon', 'webinar', 'conference', 'training', 'course', 'meetup', 'others']
+    keywords = [hackathon, webinar, conference, training, course, meetup]
 
-online = ['Без города', 'без города', 'online', 'онлайн', 'Online', 'Онлайн']
+    online = ['Без города', 'без города', 'online', 'онлайн', 'Online', 'Онлайн']
 
-for i in range(len(events_df)):
-    descr = events_df['description'][i]
-    name = events_df['name'][i]
-    city = events_df['city'][i]
-    if pandas.isna(city) or any(word in city for word in online):
-        events_df['city'][i] = 'online'
-    for j in range(len(keywords)):
-        if any(word in name for word in keywords[j]):
-            events_df['type'][i] = types[j]
-        elif not pandas.isna(descr):
-            if any(word in descr for word in keywords[j]):
+    for i in range(len(events_df)):
+        descr = events_df['description'][i]
+        name = events_df['name'][i]
+        city = events_df['city'][i]
+        if pandas.isna(city) or any(word in city for word in online):
+            events_df['city'][i] = 'online'
+        for j in range(len(keywords)):
+            if any(word in name for word in keywords[j]):
                 events_df['type'][i] = types[j]
-events_df['type'] = events_df['type'].fillna(types[-1])
+            elif not pandas.isna(descr):
+                if any(word in descr for word in keywords[j]):
+                    events_df['type'][i] = types[j]
+    events_df['type'] = events_df['type'].fillna(types[-1])
 
-events_df.to_csv('events18102020_withtypes.csv')
+    csv_out = 'events18102020_withtypes.csv'
+    events_df.to_csv(csv_out, index=False, index_label=False)
+
+
+csv_in = 'events18102020.csv'
+classificate_types_and_format(csv_in)
--- preprocessing_events_data.py
+++ preprocessing_events_data.py
@@ -1,48 +1,59 @@
-import pandas
-import pymorphy2
-import numpy as np
-import nltk
-nltk.download("stopwords")
-from nltk.corpus import stopwords
-import langdetect
+def preprocess_events_data(csv_in):
+    import pandas
+    import pymorphy2
+    import numpy as np
+    import nltk
+    from nltk.corpus import stopwords
+    nltk.download("stopwords")
 
-events_df = pandas.read_csv('meetup_tech_18102020_tz_fixed.csv')
+    events_df = pandas.read_csv(csv_in)
 
-events_df['description'] = events_df['description'].replace(r'https?:\/\/[^\s]+', '', regex=True)
-events_df['description'] = events_df['description'].str.lower()
-events_df['description'] = events_df['description'].replace('ё', 'е', regex=True)
-events_df['description'] = events_df['description'].replace(r'\w*январ\w*|\w*феврал\w*|\w*март\w*|\w*апрел\w*|\w*июн\w*|\w*июл\w*|\w*август\w*|\w*сентябр\w*|\w*октябр\w*|\w*ноябр\w*|\w*декабр\w*|\bма.\b', ' ', regex=True)
-events_df['description'] = events_df['description'].replace(r'[^а-яА-Яa-zA-Z]', ' ', regex=True)
+    events_df['description'] = events_df['description'].replace(r'https?:\/\/[^\s]+', '', regex=True)
+    events_df['description'] = events_df['description'].str.lower()
+    events_df['description'] = events_df['description'].replace('ё', 'е', regex=True)
+    events_df['description'] = events_df['description'].replace(
+        r'\w*январ\w*|\w*феврал\w*|\w*март\w*|\w*апрел\w*|\w*июн\w*|\w*июл\w*|\w*август\w*|\w*сентябр\w*|\w*октябр\w*|\w*ноябр\w*|\w*декабр\w*|\bма.\b',
+        ' ', regex=True)
+    events_df['description'] = events_df['description'].replace(r'[^а-яА-Яa-zA-Z]', ' ', regex=True)
 
-events_df['name'] = events_df['name'].str.lower()
-events_df['name'] = events_df['name'].replace('ё', 'е', regex=True)
-events_df['name'] = events_df['name'].replace(r'\w*январ\w*|\w*феврал\w*|\w*март\w*|\w*апрел\w*|\w*июн\w*|\w*июл\w*|\w*август\w*|\w*сентябр\w*|\w*октябр\w*|\w*ноябр\w*|\w*декабр\w*|\bма.\b', ' ', regex=True)
-events_df['name'] = events_df['name'].replace(r'[^а-яА-Яa-zA-Z]', ' ', regex=True)
+    events_df['name'] = events_df['name'].str.lower()
+    events_df['name'] = events_df['name'].replace('ё', 'е', regex=True)
+    events_df['name'] = events_df['name'].replace(
+        r'\w*январ\w*|\w*феврал\w*|\w*март\w*|\w*апрел\w*|\w*июн\w*|\w*июл\w*|\w*август\w*|\w*сентябр\w*|\w*октябр\w*|\w*ноябр\w*|\w*декабр\w*|\bма.\b',
+        ' ', regex=True)
+    events_df['name'] = events_df['name'].replace(r'[^а-яА-Яa-zA-Z]', ' ', regex=True)
 
-morph = pymorphy2.MorphAnalyzer()
+    morph = pymorphy2.MorphAnalyzer()
 
-stopwords = stopwords.words("russian")
-stopwords.extend(['что', 'это', 'весь', 'этот', 'привет', 'так', 'вот', 'как', 'ссылка', 'регистрация', 'приглашать', 'еще'])
-descr_array = []
-names_array = []
+    stopwords = stopwords.words("russian")
+    stopwords.extend(
+        ['что', 'это', 'весь', 'этот', 'привет', 'так', 'вот', 'как', 'ссылка', 'регистрация', 'приглашать', 'еще',
+         'год', 'спикер', 'вопрос', 'тема', 'наш', 'свой', 'время', 'который', 'выступить', 'встреча', 'мочь',
+         'jenkins', 'masked', 'ваш', 'epam', 'место', 'spb'])
+    descr_array = []
+    names_array = []
+    for i in range(len(events_df)):
+        descr = events_df['description'][i]
+        if not pandas.isna(descr):
+            descr = [morph.parse(word)[0].normal_form for word in descr.split() if
+                     morph.parse(word)[0].tag.POS not in {'INTJ', 'PRCL', 'CONJ', 'PREP', 'NPRO'}
+                     and morph.parse(word)[0].normal_form not in stopwords]
+            if not descr:
+                descr = np.nan
+            else:
+                descr = ' '.join(descr)
+        descr_array.append(descr)
+        name = events_df['name'][i]
+        if not pandas.isna(name):
+            name = [morph.parse(word)[0].normal_form for word in name.split() if
+                    morph.parse(word)[0].tag.POS not in {'INTJ', 'PRCL', 'CONJ', 'PREP', 'NPRO'}]
+            name = ' '.join(name)
+        names_array.append(name)
+    events_df['name'] = names_array
+    events_df['description'] = descr_array
+    csv_out = 'events18102020.csv'
+    events_df.to_csv(csv_out, index=False, index_label=False)
 
-for i in range(len(events_df)):
-    descr = events_df['description'][i]
-    if not pandas.isna(descr):
-        descr = [morph.parse(word)[0].normal_form for word in descr.split() if morph.parse(word)[0].tag.POS not in {'INTJ', 'PRCL', 'CONJ', 'PREP', 'NPRO'}
-                 and morph.parse(word)[0].normal_form not in stopwords and langdetect.detect(word) == 'ru']
-        if not descr:
-            descr = np.nan
-        else:
-            descr = ' '.join(descr)
-    descr_array.append(descr)
-    name = events_df['name'][i]
-    if not pandas.isna(name):
-        name = [morph.parse(word)[0].normal_form for word in name.split() if morph.parse(word)[0].tag.POS not in {'INTJ', 'PRCL', 'CONJ', 'PREP', 'NPRO'}
-                and morph.parse(word)[0].normal_form not in stopwords]
-        name = ' '.join(name)
-    names_array.append(name)
-events_df['name'] = names_array
-events_df['description'] = descr_array
 
-events_df.to_csv('events18102020.csv')
+csv_in = 'meetup_tech_18102020_tz_fixed.csv'
+preprocess_events_data(csv_in)
