0d6d51f9ea93b60e9df2ec917270141adbcda706
alinadmb <43730690+alinadmb@users.noreply.github.com>
2020-10-24 16:20:08 +0300

    added names as stopwords

--- preprocessing_events_data.py
+++ preprocessing_events_data.py
@@ -30,7 +30,12 @@ def preprocess_events_data(csv_in, csv_out):
     stopwords.extend(
         ['что', 'это', 'весь', 'этот', 'привет', 'так', 'вот', 'как', 'ссылка', 'регистрация', 'приглашать', 'еще',
          'год', 'спикер', 'вопрос', 'тема', 'наш', 'свой', 'время', 'который', 'выступить', 'встреча', 'мочь',
-         'jenkins', 'masked', 'ваш', 'epam', 'место', 'spb', 'новый', 'александр'])
+         'jenkins', 'masked', 'ваш', 'epam', 'место', 'spb', 'новый'])
+
+    rus_names = list(pandas.read_csv('russian_names.csv')['Name'].values)
+    rus_surnames = list(pandas.read_csv('russian_surnames.csv')['Surname'].values)
+    for_names = list(pandas.read_csv('foreign_names.csv')['name'].values)
+    all_names = rus_names + rus_surnames + for_names
 
     descr_array = []
     names_array = []
@@ -39,18 +44,21 @@ def preprocess_events_data(csv_in, csv_out):
         if not pandas.isna(descr):
             descr = [morph.parse(word)[0].normal_form for word in descr.split() if
                      morph.parse(word)[0].tag.POS not in {'INTJ', 'PRCL', 'CONJ', 'PREP', 'NPRO'}
-                     and morph.parse(word)[0].normal_form not in stopwords]
+                     and morph.parse(word)[0].normal_form not in stopwords
+                     and morph.parse(word)[0].normal_form not in all_names]
             if not descr:
                 descr = np.nan
             else:
                 descr = ' '.join(descr)
         descr_array.append(descr)
+
         name = events_df['normalized_name'][i]
         if not pandas.isna(name):
             name = [morph.parse(word)[0].normal_form for word in name.split() if
                     morph.parse(word)[0].tag.POS not in {'INTJ', 'PRCL', 'CONJ', 'PREP', 'NPRO'}]
             name = ' '.join(name)
         names_array.append(name)
+
     events_df['normalized_name'] = names_array
     events_df['normalized_description'] = descr_array
     events_df = events_df[['name', 'normalized_name', 'event_time', 'description', 'normalized_description', 'city', 'category']]
@@ -60,5 +68,4 @@ def preprocess_events_data(csv_in, csv_out):
 
 csv_in = 'meetup_tech_18102020_tz_fixed.csv'
 csv_out = 'events18102020.csv'
-
 preprocess_events_data(csv_in, csv_out)
